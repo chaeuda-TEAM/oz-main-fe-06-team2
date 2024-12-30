6c059c044035de53592f69f25156bafaa79e88a0
Ju-MINJAE <juminjae98@gmail.com>
2024-12-30 23:50:45 +0900

    [feat] isAuthenticated 상태에 따라 UI 변경

--- src/containers/NavContainer/NavContainer.tsx
+++ src/containers/NavContainer/NavContainer.tsx
@@ -1,12 +1,33 @@
+'use client';
+
 import Link from 'next/link';
 import React from 'react';
+import useAuthStore from '@/stores/authStore';
+import { useRouter } from 'next/navigation';
 
 const NavContainer: React.FC = () => {
+  const router = useRouter();
+  const { isAuthenticated, logout } = useAuthStore();
+
+  const handleChatClick = () => {
+    if (!isAuthenticated) {
+      alert('로그인 후 이용 가능합니다.');
+      router.push('/auth/signIn');
+    } else {
+      router.push('/chat');
+    }
+  };
+
+  const handleLogout = () => {
+    logout();
+    router.push('/');
+  };
+
   return (
     <header className="fixed w-full bg-white z-10 shadow-md">
       <nav className="flex items-center justify-between h-[80px] px-10">
         <div className="mr-10 text-kick text-2xl font-bold">
-          <Link href={'/'}>채우다 로고</Link>
+          <Link href="/">채우다 로고</Link>
         </div>
         <ul className="flex space-x-8">
           <li className="hover:text-kick">
@@ -21,13 +42,29 @@ const NavContainer: React.FC = () => {
         </ul>
 
         <div className="flex items-center space-x-4 ml-auto">
-          <button className="hover:text-kick">
-            <Link href="/chat">채팅</Link>
-          </button>
-          <span className="text-gray-500">|</span>
-          <button className="hover:text-kick">
-            <Link href={'/auth/signIn'}>로그인/회원가입</Link>
-          </button>
+          {isAuthenticated ? (
+            <>
+              <button onClick={handleChatClick} className="hover:text-kick">
+                채팅
+              </button>
+              <span className="text-gray-500">|</span>
+              <button className="hover:text-kick">마이페이지</button>
+              <span className="text-gray-500">|</span>
+              <button onClick={handleLogout} className="hover:text-kick">
+                로그아웃
+              </button>
+            </>
+          ) : (
+            <>
+              <button onClick={handleChatClick} className="hover:text-kick">
+                채팅
+              </button>
+              <span className="text-gray-500">|</span>
+              <button className="hover:text-kick">
+                <Link href="/auth/signIn">로그인/회원가입</Link>
+              </button>
+            </>
+          )}
         </div>
       </nav>
     </header>
