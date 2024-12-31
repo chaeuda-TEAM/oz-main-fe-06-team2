4d7981baf5f42c9891f1b7a0b452134cf7f26601
hcartist <hcartist569@gmail.com>
2024-12-30 22:05:37 -0800

    [chore] 구글버튼 로직 수정

--- src/app/auth/google/callback/route.ts
+++ src/app/auth/google/callback/route.ts
@@ -4,10 +4,6 @@ export async function GET(req: NextRequest) {
   try {
     const searchParams = req.nextUrl.searchParams;
     const code = searchParams.get('code');
-    console.log('Received code:', code);
-    
-    // API 요청 전 로그
-    console.log('Attempting to call backend API with code');
     
     if (!code) {
       return NextResponse.json({ error: 'No code provided' }, { status: 400 });
@@ -19,9 +15,6 @@ export async function GET(req: NextRequest) {
         'Content-Type': 'application/json',
       }
     });
-
-    // 응답 상태 로그
-    console.log('Backend response status:', response.status);
     
     if (!response.ok) {
       console.error('Backend request failed with status:', response.status);
@@ -29,7 +22,6 @@ export async function GET(req: NextRequest) {
     }
 
     const data = await response.json();
-    console.log('Received data from backend:', data);
     
     if (data.success) {
       // is_active가 false일 경우 회원가입 페이지로, true일 경우 원래 리다이렉트 URL로 이동
--- src/components/GoogleBtn.tsx
+++ src/components/GoogleBtn.tsx
@@ -1,15 +1,7 @@
-const googleLogin = () => {
-  window.location.href = `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/google/login/dev`;
-};
-
 const GoogleBtn = () => {
-  const handleGoogleLogin = async () => {
-    try {
-      await googleLogin();
-      // console.log('로그인 성공:', result);
-    } catch (error) {
-      console.error('로그인 처리 중 오류 발생:', error);
-    }
+  
+  const handleGoogleLogin = () => {
+    window.location.href = `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/google/login/dev`;
   };
 
   return (
