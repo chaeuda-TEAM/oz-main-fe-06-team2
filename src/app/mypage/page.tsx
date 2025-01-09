df9c44e4d952b69896ebdf9dcc2113f5f0967f44
hcartist <hcartist569@gmail.com>
2025-01-09 08:53:18 -0800

    충돌 커밋

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -3,9 +3,10 @@
 import { sendWithdrawRequest } from '@/api/auth';
 import useAuthStore from '@/stores/authStore';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 const MyPage = () => {
-  const { user, logout } = useAuthStore();
+  const { user, logout, socialUser } = useAuthStore();
   const router = useRouter();
 
   // TODO: confirm 컴포넌트 제작
@@ -24,22 +25,27 @@ const MyPage = () => {
     }
   };
 
+useEffect(() => {
+  console.log(socialUser);
+  console.log(1);
+}, [socialUser]);
+
   return (
     <div className="max-w-md mx-auto p-6">
       <h2 className="text-3xl font-light text-kick mb-4">마이페이지</h2>
       <label className="block text-sm font-medium text-gray-700 mb-1">이메일</label>
       <p className="mb-4 p-2 bg-gray-100 border border-gray-300 rounded text-gray-600">
-        {user?.email || '이메일 정보 없음'}
+        {user?.email || socialUser?.email || '이메일 정보 없음'}
       </p>
 
       <label className="block text-sm font-medium text-gray-700 mb-1">이름</label>
       <p className="mb-4 p-2 bg-gray-100 border border-gray-300 rounded text-gray-600">
-        {user?.username || '이름 정보 없음'}
+        {user?.username || socialUser?.username || '이름 정보 없음'}
       </p>
 
       <label className="block text-sm font-medium text-gray-700 mb-1">전화번호</label>
       <p className="mb-6 p-2 bg-gray-100 border border-gray-300 rounded text-gray-600">
-        {user?.phone_number || '전화번호 정보 없음'}
+        {user?.phone_number || socialUser?.phone_number || '전화번호 정보 없음'}
       </p>
 
       <button className="w-full py-2 bg-kick text-white font-semibold">정보 수정</button>
