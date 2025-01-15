204b8788469e335d7aa947e7fb1e707a2148a661
Ju-MINJAE <juminjae98@gmail.com>
2025-01-15 21:18:52 +0900

    [style] apply favicon, logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ import Footer from '@/components/Footer';
 export const metadata: Metadata = {
   title: '채우다',
   description: '전국 빈집을 채워보자',
+  icons: {
+    icon: '/icon.svg',
+  },
 };
 
 const pretendard = localFont({
@@ -25,7 +28,7 @@ export default function RootLayout({
     <html lang="ko" className={`${pretendard.variable} w-full h-full`}>
       <body className={`${pretendard.className} flex flex-col w-full h-full overflow-hidden`}>
         <NavContainer />
-        <div className='flex flex-col felx-1 overflow-scroll w-full h-full'>
+        <div className="flex flex-col felx-1 overflow-scroll w-full h-full">
           <main className="flex flex-1 w-full">{children}</main>
           <Footer />
         </div>
