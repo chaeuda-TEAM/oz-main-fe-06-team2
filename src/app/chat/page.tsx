28912510c0055b22d4f702d47deaf6ed5e7db868
hcartist <hcartist569@gmail.com>
2025-01-14 23:32:23 -0800

    [design] 마이페이지, 채팅 로딩중 화면 높이 늘어나는 에러 해결

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -55,11 +55,11 @@ const ChatPage = () => {
   const selectedChat = chatList.find(chat => chat.id === selectedChatId);
 
   if (isLoading) {
-    return <div className="flex justify-center items-center h-screen w-full">Loading...</div>;
+    return <div className="flex justify-center items-center w-full">Loading...</div>;
   }
 
   if (error) {
-    return <div className="flex justify-center items-center h-screen text-red-500 w-full">{error}</div>;
+    return <div className="flex justify-center items-center text-red-500 w-full">{error}</div>;
   }
 
   return (
